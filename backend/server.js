6d7bce7926688e09cc169fdc985c2448c8a86b0f
OmarHamdann <omar.haamdan@gmail.com>
2022-04-11 19:01:56 +0300

    create server and route

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+
+
+
+const app = express();
+
+//built-in middleware
+app.use(cors());
+app.use(express.json());
+
+//routers
+const usersRouter = require("./routes/users");
+
+app.use("/users", usersRouter);
+
+
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`SERVER WORKING ON PORT: ${PORT}`);
+});
