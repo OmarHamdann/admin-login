0096a3f6c2f615b4e36c1f80b0be220e7b8012f4
OmarHamdann <omar.haamdan@gmail.com>
2022-04-12 00:35:20 +0300

    login route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,11 +3,13 @@ const express = require("express");
 const { createNewUser, getAllUsers,updateUserById,deleteUserById } = require("../controllers/users");
 
 const usersRouter = express.Router();
+const loginRouter = express.Router();
 
 usersRouter.post("/", createNewUser);
 usersRouter.get("/", getAllUsers);
 usersRouter.put("/:id", updateUserById);
 usersRouter.delete("/:id", deleteUserById);
+
 
 
 module.exports = usersRouter;
