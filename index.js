19a5642e4d5a8a79574098d8cda93ae13ef84394
OmarHamdann <omar.haamdan@gmail.com>
2022-04-12 20:50:00 +0300

    fix bug

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use("/login", loginRouter);
 
 //run server in port 5000
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 //for heroku deployment
 if(process.env.NODE_ENV === 'production'){
   app.use(express.static('client/build'));
